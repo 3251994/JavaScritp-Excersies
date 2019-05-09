b5bceb8c195a1b004f32faf02357453f23a6e67c
Anitson TT <anitsonttsat@gmail.com>
2019-05-09 19:08:59 +0530

    First commit

---
+++ myScript.js
@@ -0,0 +1,129 @@
+//alert("Method #3 : Welcome from External FIle!");
+
+/* console.log("This is log message");
+
+console.error("This is an error");
+
+console.info("This is adding an info");
+
+console.warn("This is your first warning"); */
+
+/* var myFirstVariable;
+
+document.write(myFirstVariable);
+
+var myName;
+
+myName = "Anitson T T";
+
+document.write(myName);
+
+document.write("<br>");
+
+var yourName = "Anitson T T";
+
+document.write(yourName);
+
+myCar = "Benz";
+
+var a, b, c;
+
+var a = 10, b = 20, c = 30; */
+
+/* var x = 100, y = 20;
+
+var a = x + y;
+
+document.write("Addition: ");
+document.write(a); */
+
+
+//var studentAge = 16;
+
+/* if (studentAge == 18)
+{
+	document.write("Student age value is equal to 18 <br>");
+} */
+
+/* if(studentAge === 18)
+{
+	document.write("value and type are same and is equal to 18 <br>");
+} */
+
+/* if (studentAge!= 18)
+{
+	document.write("not equal to 18 <br>");
+}
+
+if (studentAge !== 18)
+{
+	document.write("value or type not same <br>");
+} */
+
+/* if (studentAge > 18)
+{
+	document.write("value is greater than 18 <br>");
+}
+
+if (studentAge < 18)
+{
+	document.write("value is less than 18 <br>");
+}
+if (studentAge >= 18)
+{
+	document.write("value is greater than or equal to 18 <br>");
+}
+
+if (studentAge <= 18)
+{
+	document.write("value is less than or equal to  18 <br>");
+} */
+
+/* var studentAge = 18;
+var studentGender = "M";
+
+if ((studentAge >= 18 )&& (studentGender == "M"))
+{
+	document.write("age is greater than or equal to 18 and gender is male <br>");
+}
+
+if ((studentAge >= 18 )||(studentGender == "M"))
+{
+	document.write("age is greater than or equal to 18 or gender is male <br>");
+}
+if (!(studentAge >18))
+{
+	document.write("Student age is less than 18");
+}  */
+
+var x = 15,y = 7 ,z = 3;
+
+if ((x >y) && (x > z))
+   {
+	document.write(" Largest number is " +x+ ");
+	}else if ( (y > x) && (y > z) )
+	{
+		document.write(" Largest number is " +y+ ");
+	}
+	else
+	{
+		document.write(" Largest number is " +z+ ");
+	}
+	
+var x = 15, y = 7, z  = 3;
+    
+    if ( (x > y) && (x > z) ) {
+      
+      document.write("The largest number is " + x + ");
+      
+    } else if ( (y > x) && (y > z) ) {
+      
+      document.write("The largest number is " + y + ");
+      
+    } else {
+      
+      document.write("The largest number is " + z + ");
+      
+    }
+  
+
