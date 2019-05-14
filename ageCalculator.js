160688bf374cbc21749685836d5e21e2b61635ee
Anitson T T <anitsonttsat@gmail.com>
2019-05-14 17:15:36 +0530

    Add files via upload

---
+++ ageCalculator.js
@@ -0,0 +1,8 @@
+(function(){alert("Welcome to Age Calculator");})();
+
+function  calculateAge( birthYear, currentYear ){
+
+ var age =  (currentYear - birthYear);
+ document.write(" Age :" + age);
+ }
+ calculateAge(1994,2019);
