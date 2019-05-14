8963f6ed8ed3a498e712f7d502f6a71c51e8bf14
Anitson T T <anitsonttsat@gmail.com>
2019-05-14 15:56:55 +0530

    Add files via upload

---
+++ largestNum.js
@@ -0,0 +1,29 @@
+var num1 = 15,num2 = 7 ,num3 = 3;
+
+if ((num1 >num2) && (num1 > num3))
+   {
+	document.write(" Largest number is " +num1+ ");
+	}else if ( (num2 > num1) && (num2 > num3) )
+	{
+		document.write(" Largest number is " +num2+ ");
+	}
+	else
+	{
+		document.write(" Largest number is " +num3+ ");
+	}
+	
+var num1 = 15, num2 = 7, num3  = 3;
+    
+    if ( (num1 > num2) && (num1 > num3) ) {
+      
+      document.write("The largest number is " + num1 + ");
+      
+    } else if ( (num2 > num1) && (num2 > num3) ) {
+      
+      document.write("The largest number is " + num2 + ");
+      
+    } else {
+      
+      document.write("The largest number is " + num3 + ");
+      
+    } 
